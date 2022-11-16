2bcc1639ba569613e4b76666aa6ecedc70b0dea2
akrn1004 <117414672+akrn1004@users.noreply.github.com>
2022-11-16 12:47:57 +0900

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,61 @@
-window.addEventListener('load', () => {
+$(document).ready(function () {
+  $('.allBtn').mouseover(function () {
+    $('.menu').addClass('down')
+  });
+  $('.menu').mouseleave(function () {
+    $('.menu').removeClass('down')
+  });
+  $('.menu li').mouseover(function () {
+    $(this).addClass('on')
+  });
+  $('.menu li').mouseleave(function () {
+    $(this).removeClass('on')
+  });
+
+  $('.nameBtn li').mouseover(function () {
+    $(this).addClass('backAct')
+  });
+  $('.nameBtn li').mouseleave(function () {
+    $(this).removeClass('backAct')
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   $(document).scroll(function () {
-    var scroll_X = $(this).scrollTop();
-    console.log(scroll_X)
-    if (scroll_X >= 750) {
+    var scroll_X2 = $(this).scrollTop();
+    console.log(scroll_X2)
+    if (scroll_X2 >= 750) {
 
       $('.school li').addClass('schoolAct');
+
     }
-    else if (scroll_X < 740) {
+    else if (scroll_X2 < 740) {
       $('.school li').removeClass('schoolAct');
 
 
